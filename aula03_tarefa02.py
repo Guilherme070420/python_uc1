5b2edf0cc2d4b0e4b69258ae6ac3d707db28d049
Guilherme070420 <guilhermeytb.08@gmail.com>
2025-03-07 20:47:00 -0300

    Add files via upload
    aula03_tarefa.py

---
+++ aula03_tarefa02.py
@@ -0,0 +1,17 @@
+a = 10
+b = 3
+soma = a + b
+subtração= a - b
+multiplicação= a * b
+Divisao_real= a / b
+Divisao_inteira= a // b
+Módulo= a % b
+exponenciação = a ** b
+
+print(f"o resultado da soma e {soma}")
+print(f"o resultado da subtração e {subtração}")
+print(f"o resultado da multiplicação e {multiplicação}")
+print(f"o resultado da divisão real e {Divisao_real}")
+print(f"o resultado da divisão inteira e {Divisao_inteira}")
+print(f"o resultado da Módulo e {Módulo}")
+print(f"o resultado da exponenciação e {exponenciação}")
