fdb5381c4b269ca16d9404663c3cf6c8e7cff84f
Guilherme070420 <guilhermeytb.08@gmail.com>
2025-03-07 21:46:45 -0300

    Add files via upload
    aula04_tarefa01.py

---
+++ aula04_tarefa01.py
@@ -0,0 +1,23 @@
+x = 10
+print(f"o valor inicial de x e {x}")
+
+x+=5
+print(f"o valor da soma  de x e {x}")
+
+x-=3
+print(f"o valor da subtração de x e {x}")
+
+x*=2
+print(f"o valor da multiplicação de x e{x}")
+
+x/=4
+print(f"o valor da divisão de x e {x}")
+
+x//=3
+print(f"o valor da divisão_inteira de x e {x}")
+
+x %=2
+print(f"o valor do modulo de x e {x} ")
+
+x**=3
+print(f"o valor da exponenciação de x e {x}")
