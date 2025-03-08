401846e633feeb05e3194e906f60e669df79c504
Guilherme070420 <guilhermeytb.08@gmail.com>
2025-03-07 21:20:37 -0300

    Add files via upload
    aula03_desafio03.py

---
+++ aula03_desafio03.py
@@ -0,0 +1,9 @@
+a = input("me de a primeira nota :") 
+a = float(a)   
+b = input("me de a segunda  nota :")
+b = float(b)   
+c = input("me de a terceira nota :") 
+c = float(c)  
+
+media = (a+ b + c)/3
+print(f"a medida das notas e {media:.2f}")
