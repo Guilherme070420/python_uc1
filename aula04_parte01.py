f9bf93097db5450ba0022dbbeeaa68870e983beb
Guilherme070420 <guilhermeytb.08@gmail.com>
2025-03-12 20:18:23 -0300

    aula04_parte1.py

---
+++ aula04_parte01.py
@@ -0,0 +1,8 @@
+input("me fale a sua idade:")
+idade = 18
+if idade >= 18:
+   print("você e maior de idade ")
+elif idade >= 16:
+    print("você e adolescente de idade")   
+else:
+    print("você e criança")
