704389f135b2458ab33f2b024917b2adbbbb45ed
Guilherme070420 <guilhermeytb.08@gmail.com>
2025-03-12 20:19:16 -0300

    aula04_-desafio.4.4.1.py

---
+++ aula04_-desafio.4.4.1.py
@@ -0,0 +1,10 @@
+numero=input("me diga um mumero:")
+numero=float(numero)
+#numero = 15
+if numero < 0:
+    print("o numero e negativo")
+elif numero == 0:
+    print("o numero e zero")  
+else:
+    print("o numero e posi1tivo") 
+   
