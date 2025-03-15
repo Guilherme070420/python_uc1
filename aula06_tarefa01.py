555fc4f85e0b39bd78767a5ce4851da726bb5e9b
Guilherme070420 <guilhermeytb.08@gmail.com>
2025-03-14 21:37:27 -0300

    aula06_tarefa01.py

---
+++ aula06_tarefa01.py
@@ -0,0 +1,13 @@
+frutas = ["maçã", "banana", "cereja"]
+for item in frutas:
+    print(frutas)
+input=("\n   PRESSIONAR ENTER !!!\n")
+print(f"\t {item.upper()}")
+
+
+contagem = (9,8,7,6,5,4,3,2,1,0)
+for item in contagem:
+    print(f"{item}... ")
+
+for i in range(100):
+    print(i)
