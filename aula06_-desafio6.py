b118804288885745ae683c9fbd46b09490672353
Guilherme070420 <guilhermeytb.08@gmail.com>
2025-03-20 19:01:50 -0300

    aula06_-desafio6.py
    aula06_-desafio6.py

---
+++ aula06_-desafio6.py
@@ -0,0 +1,8 @@
+soma = 0
+
+# Loop para somar os números de 1 a 100
+for numero in range(1, 101):
+    soma += numero  # Adiciona o número atual à soma
+
+# Imprime o resultado
+print(f"A soma dos números de 1 a 100 é: {soma}")
